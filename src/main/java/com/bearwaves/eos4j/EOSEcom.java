87739f9fd736ecb09f604532a4168b5f198541b9
Joel Auterson <joel.auterson@gmail.com>
2024-01-16 09:44:42 +0000

    Add copyOfferByIndex

--- src/main/java/com/bearwaves/eos4j/EOSEcom.java
+++ src/main/java/com/bearwaves/eos4j/EOSEcom.java
@@ -1,5 +1,7 @@
 package com.bearwaves.eos4j;
 
+import java.util.Date;
+
 public class EOSEcom {
 
     private final long handle;
@@ -14,6 +16,72 @@ public class EOSEcom {
 
     public int getOfferCount(GetOfferCountOptions options) {
         return EOSEcomNative.getOfferCount(handle, options);
+    }
+
+    public CatalogOffer copyOfferByIndex(CopyOfferByIndexOptions options) throws EOSException {
+        return EOSEcomNative.copyOfferByIndex(handle, options);
+    }
+
+    public static class CatalogOffer extends EOSHandle {
+        public final int serverIndex;
+        public final String catalogNamespace;
+        public final String id;
+        public final String titleText;
+        public final String descriptionText;
+        public final String longDescriptionText;
+        public final String currencyCode;
+        public final int priceResult;
+        public final int discountPercentage;
+        public final Date expiration;
+        public final int purchaseLimit;
+        public final boolean availableForPurchase;
+        public final long originalPrice;
+        public final long currentPrice;
+        public final int decimalPoint;
+        public final Date releaseDate;
+        public final Date effectiveDate;
+
+        public CatalogOffer(long ptr,
+                            int serverIndex,
+                            String catalogNamespace,
+                            String id,
+                            String titleText,
+                            String descriptionText,
+                            String longDescriptionText,
+                            String currencyCode,
+                            int priceResult,
+                            int discountPercentage,
+                            Date expiration,
+                            int purchaseLimit,
+                            boolean availableForPurchase,
+                            long originalPrice,
+                            long currentPrice,
+                            int decimalPoint,
+                            Date releaseDate,
+                            Date effectiveDate) {
+            super(ptr);
+            this.serverIndex = serverIndex;
+            this.catalogNamespace = catalogNamespace;
+            this.id = id;
+            this.titleText = titleText;
+            this.descriptionText = descriptionText;
+            this.longDescriptionText = longDescriptionText;
+            this.currencyCode = currencyCode;
+            this.priceResult = priceResult;
+            this.discountPercentage = discountPercentage;
+            this.expiration = expiration;
+            this.purchaseLimit = purchaseLimit;
+            this.availableForPurchase = availableForPurchase;
+            this.originalPrice = originalPrice;
+            this.currentPrice = currentPrice;
+            this.decimalPoint = decimalPoint;
+            this.releaseDate = releaseDate;
+            this.effectiveDate = effectiveDate;
+        }
+
+        public void release() {
+            EOSEcomNative.releaseCatalogOffer(ptr);
+        }
     }
 
     public static class QueryOffersOptions {
@@ -31,6 +99,16 @@ public class EOSEcom {
 
         public GetOfferCountOptions(EOS.EpicAccountId localUserId) {
             this.localUserId = localUserId;
+        }
+    }
+
+    public static class CopyOfferByIndexOptions {
+        public final EOS.EpicAccountId localUserId;
+        public final int offerIndex;
+
+        public CopyOfferByIndexOptions(EOS.EpicAccountId localUserId, int offerIndex) {
+            this.localUserId = localUserId;
+            this.offerIndex = offerIndex;
         }
     }
 
